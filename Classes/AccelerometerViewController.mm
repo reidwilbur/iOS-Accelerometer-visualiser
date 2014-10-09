ccd7927e1c8949b5a2985baae9e4194a7877e5ba
Ranqi Zhu <rouge@spotify.com>
2014-10-09 13:28:40 -0400

    tunes

--- Classes/AccelerometerViewController.mm
+++ Classes/AccelerometerViewController.mm
@@ -133,7 +133,7 @@ void getDataFromFile(NSString *filePath, std::vector<std::vector<float> > &data,
 CvSVMParams getConfiguredSVMParams(){
     CvSVMParams params;
     params.svm_type = CvSVM::C_SVC;
-    params.kernel_type = CvSVM::LINEAR;
+    params.kernel_type = CvSVM::POLY;
     params.gamma = 20;
     params.degree = 1;
     params.coef0 = 0;
